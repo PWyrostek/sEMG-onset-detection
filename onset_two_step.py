bab3181f41aa0b400bce636622bf8c14d0e22ad7
Mateusz Magda <realgs@gmail.com>
2020-10-01 21:55:38 +0200

    add fault tolerance for Two Stage, cleanup Teager Kaiser

--- onset_two_step.py
+++ onset_two_step.py
@@ -5,6 +5,7 @@ from onset_sign_changes import onset_sign_changes
 
 
 def onset_two_step_alg(data, W_1, k_1, d_1, h_2, W_2, M_2):
+    first_stage_fault_tolerance = 100
     left, right = onset_sign_changes(data, W_1, k_1, d_1)  # 200,1,0.01
-    result = onset_AGLRstep_two_step(data, h_2, W_2, M_2, left - W_1, right)  # 20,15,20
+    result = onset_AGLRstep_two_step(data, h_2, W_2, M_2, left - first_stage_fault_tolerance, right)  # 20,15,20
     return result
