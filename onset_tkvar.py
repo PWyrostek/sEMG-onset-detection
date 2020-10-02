c4f8f2b6738319f447843e69c0eff247cd9a5df4
Mateusz Magda <realgs@gmail.com>
2020-10-02 19:11:07 +0200

    fixes for TKVar

--- onset_tkvar.py
+++ onset_tkvar.py
@@ -4,7 +4,8 @@ import numpy as np
 def onset_TKVar(data, W, g):
     var = np.var(data[0:W])
     h = var * g
-    for i in range(0, len(data)):
+
+    for i in range(0, len(data) - W):
         if np.var(data[i:i + W]) > h:
-            return i + 3 * W / 4
+            return i + W / 2
     return -1
